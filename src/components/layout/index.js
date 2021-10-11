cab0b6a73373127f084c60586aa6eb1812dd9b0b
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-10-11 17:52:00 +0530

    nana

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./style.css";
 
-import Footer from "../footer";
+// import Footer from "../footer";
 const Index = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen h-auto">
       <div className="flex-1">{children}</div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
--- src/sections/heroSection/index.js
+++ src/sections/heroSection/index.js
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { Button } from "../../components";
-import Gif from "../../assets/images/Gif.gif";
-import Logo from "../../assets/images/logo.png";
-
+// import Gif from "../../assets/images/Gif.gif";
+// import Logo from "../../assets/images/logo.png";
 import Slider, { SliderTooltip } from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./style.css";
@@ -13,7 +12,7 @@ const Index = ({ account, mint, totalSupply, displayPrice, loadWeb3 }) => {
   const { Handle } = Slider;
 
   const handle = (props) => {
-    const { value, dragging, index, ...restProps } = props;
+    const { value, dragging, Index, ...restProps } = props;
     if (value > 0) {
       setValue(value);
     }
@@ -24,7 +23,7 @@ const Index = ({ account, mint, totalSupply, displayPrice, loadWeb3 }) => {
         overlay={`${value}`}
         visible={dragging}
         placement="top"
-        key={index}
+        key={Index}
       >
         <Handle value={value} {...restProps} />
       </SliderTooltip>
@@ -33,16 +32,9 @@ const Index = ({ account, mint, totalSupply, displayPrice, loadWeb3 }) => {
 
   return (
     <section className="py-10">
-      <div className="container lg:flex lg:flex-row-reverse gap-10 ">
-        <div className="lg:w-1/2 w-full">
-          <div className="flex justify-between mb-20 items-center">
-            <div>
-              <img
-                src={Logo}
-                alt="logo"
-                className="logo lg:hidden w-28 sm:w-40"
-              />
-            </div>
+      <div className="container ">
+        <div className=" w-full">
+          <div className="flex justify-center mb-20 items-center">
             <Button color="primary" onClick={() => loadWeb3()}>
               {account
                 ? account.slice(0, 8) +
@@ -51,16 +43,8 @@ const Index = ({ account, mint, totalSupply, displayPrice, loadWeb3 }) => {
                 : "disconnected"}
             </Button>
           </div>
-          <img
-            src={Gif}
-            alt="gif"
-            className="max-w-sm lg:max-w-md mx-auto w-full"
-          />
         </div>
-        <div className="lg:w-1/2 w-full">
-          <div className="hidden lg:block">
-            <img src={Logo} alt="logo" />
-          </div>
+        <div className=" w-full ">
           <p className="text-center sm:mx-auto  text-lg font-black mt-6  w-full  max-w-lg ">
             The Nebula Conqueror is a 10,000 NFT's series of space Avatar which
             will be usable as characters in our cosmos conquest browser P2E
@@ -71,7 +55,7 @@ const Index = ({ account, mint, totalSupply, displayPrice, loadWeb3 }) => {
             <div className="flex justify-center text-xs mb-5">
               <span>Total Nebula Conqueror Minted: {totalSupply} / 7777</span>
             </div>
-            <Slider min={0} max={20} defaultValue={1} handle={handle} />
+            <Slider min={1} max={5} defaultValue={1} handle={handle} />
             <p className="text-center text-xs mt-2">
               {account
                 ? `Mint ${value} Starfish(${(value * displayPrice).toFixed(
@@ -88,7 +72,7 @@ const Index = ({ account, mint, totalSupply, displayPrice, loadWeb3 }) => {
                 Mint Your Nebula Conqueror
               </Button>
               <p className="mt-10 text-center">
-                20 fully unique are hidden in the 10k NFTs.
+                5 fully unique are hidden in the 10k NFTs.
               </p>
               <p className="mx-auto text-center">Good luck!</p>
             </div>
